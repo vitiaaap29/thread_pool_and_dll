6b72ca2308b22ab0e70817b1260dd8383645f62a
vitiaaap29 <viktor.kyrilovich@gmail.com>
2013-12-18 21:33:36 +0400

    copy almost

--- OSiSP/OSiSP/OSiSP.cpp
+++ OSiSP/OSiSP/OSiSP.cpp
@@ -10,20 +10,55 @@
 /*Указатели на загружаемые функции*/
 void (*list) (std::wstring dirName);
 
-int loadAllLibrary(std::vector<std::wstring*> dllName);
+int loadAllLibrary(std::vector<std::wstring> dllName, std::vector<std::wstring> functionNames);
 void status();
 void exit();
 void copy();
 void size();
 
 int listFileData(TCHAR* dirName);
+DWORD WINAPI copyFile(PVOID context);
+void copy(TCHAR* nameSource, TCHAR* destinationName);
 
-int main()
+struct CopyFilesInfo
 {
+	LPCTSTR lpExistingFileName;
+	LPCTSTR lpNewFileName;
+	BOOL bFailIfExists;
+};
+
+int main(int argc, char** argv)
+{
+	if (argc < 4)
+	{
+		wprintf(TEXT("Не правильное число агрументов. Идём лесом.\n"));
+		system("pause");
+		return -1;
+	}
+
+	int min = atoi(argv[1]);
+	int max = atoi(argv[2]);
+	int timeLive = atoi(argv[3]);
+
+	if (min < 1 || max > 500 || timeLive < 0)
+	{
+		wprintf(TEXT("Аргументы имеют недопустимое значение. Идём лесом.\n"));
+		system("pause");
+		return -1;
+	}
+
 	setlocale( LC_ALL,"Russian" );
 	std::vector<std::wstring> dllNames;
 	dllNames.push_back(L"list.dll");
 
+	std::vector<std::wstring> functionNames;
+	functionNames.push_back(L"listFileData");
+
+	PTP_POOL pool = CreateThreadpool(NULL);
+	SetThreadpoolThreadMinimum(pool, min);
+	SetThreadpoolThreadMinimum(pool, max);
+
+	loadAllLibrary(dllNames, functionNames);
 	int input;
 	std::cout<<"1-List\n";
 	std::cout<<"2-Status\n";
@@ -35,35 +70,102 @@ int main()
 
 	TCHAR dirName[MAX_PATH];
 
-	switch ( input ) {
-
-	case 1:
-		std::cout<<"Vvedite polni put' k direktorii:";
-		wscanf_s(L"%s",dirName, _countof(dirName));
-		listFileData(dirName);
-		break;
-	case 2:
-		status();
-		break;
-	case 3:
-		exit();
-		break;
-	case 4:
-		copy();
-		break;
-	case 5:
-		size();
-		break;
-	default:
-		std::cout<<"Error, bad input, quitting\n";
-		break;
-	}
+	do{
+		switch ( input ) 
+		{
+			case 1:
+				std::cout<<"Vvedite polni put' k direktorii:";
+				wscanf_s(L"%s",dirName, _countof(dirName));
+				listFileData(dirName);
+				break;
+			case 2:
+				status();
+				break;
+			case 3:
+				exit();
+				break;
+			case 4:
+				copy();
+				break;
+			case 5:
+				size();
+				break;
+			default:
+				std::cout<<"Error, bad input, quitting\n";
+				break;
+		}
+	}while(input != 3);
 
 	system("pause");
 	return 0;
 
 }
 
+void copy(TCHAR* nameSource, TCHAR* destinationName)
+{
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+
+	hFind = FindFirstFile(nameSource, &ffd);
+	if (INVALID_HANDLE_VALUE == hFind) 
+	{
+		std::cout << _T("\r\nОшибка копирования\r\n") << std::endl;
+		return;
+	} 
+
+	if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+	{
+		if (CreateDirectory(destinationName, NULL))
+		{
+			wprintf(TEXT("Копируем %s в %s\n"), ffd.cFileName, destinationName);
+			WIN32_FIND_DATA nextFfd;
+			LPCWSTR nameNext = (LPCWSTR)(&std::wstring(nameSource).append(L"\\*")[0]);
+			HANDLE handleNextFile = FindFirstFile(nameNext, &nextFfd);
+			do
+			{
+				if (nextFfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				{
+					int lengthShortPartName = GetShortPathName(nextFfd.cFileName, NULL, 0);
+					TCHAR* shortName = new TCHAR[lengthShortPartName];
+					lengthShortPartName = GetShortPathName(nextFfd.cFileName, shortName, lengthShortPartName);
+					if (lengthShortPartName != 0)
+					{
+						LPWSTR nameInsideDir = (LPWSTR)(&std::wstring(destinationName).append(L"\\")[0]);
+						nameInsideDir = lstrcatW(nameInsideDir, (LPWSTR)shortName);
+						copy(nextFfd.cFileName, nameInsideDir);
+					}
+					else
+					{
+						wprintf(TEXT(" Ошибка определение короткого имени файла\n"));
+					}
+				}
+				else
+				{
+					/*filesize.LowPart = ffd.nFileSizeLow;
+					filesize.HighPart = ffd.nFileSizeHigh;
+					wprintf(TEXT("  %s   %ld bytes\n"), ffd.cFileName, filesize.QuadPart);*/
+					//тут надо ставить в очередь функцию копирования конкретных файлов
+				}
+			}
+			while (FindNextFile(hFind, &ffd) != 0); 
+		}
+		else
+		{
+			wprintf(TEXT("Ошибка создания новой директории\n"));
+		}
+	}
+	else
+	{
+		//тут надо ставить в очередь функцию копирования конкретных файлов
+	}
+}
+
+DWORD WINAPI copyFile(PVOID context)
+{
+	CopyFilesInfo* cfi = (CopyFilesInfo*)context;
+	int result = CopyFile(cfi->lpExistingFileName, cfi->lpNewFileName, cfi->bFailIfExists);
+	return result;
+}
 
 int listFileData(TCHAR* dirName)
 {
@@ -98,15 +200,15 @@ int listFileData(TCHAR* dirName)
 	return 0;
 }
 
-int loadAllLibrary(std::vector<std::wstring*> dllName)
+int loadAllLibrary(std::vector<std::wstring> dllNames, std::vector<std::wstring> functionNames)
 {
 	std::vector<void*> adress;
-	adress.push_back(list); //list
+	adress.push_back(&list); //list
 	//
 	//
 	//
 	//
-	int countLibrary = dllName.size();
+	int countLibrary = dllNames.size();
 
 	for (int i = 0; i < countLibrary; i++)
 	{
@@ -114,7 +216,7 @@ int loadAllLibrary(std::vector<std::wstring*> dllName)
 		// Указатель на функцию
 		//void (*adress) (void);
 
-		LPCWSTR name = (LPCWSTR)dllName[i];
+		LPCWSTR name = const_cast<LPCWSTR>(&dllNames[i][0]);
 		// Загружаем динамически подключаемую библиотеку
 		hDll = LoadLibrary(name);
 		if(!hDll)
@@ -123,7 +225,7 @@ int loadAllLibrary(std::vector<std::wstring*> dllName)
 			return GetLastError();
 		}
 		// Настраиваем адрес функции
-		adress[i] = (void (*)(void))GetProcAddress(hDll, "HelloWorld");
+		adress[i] = (void (*)(void))GetProcAddress(hDll, (LPCSTR)(&functionNames.at(i)[0]));
 		if(!adress[i])
 		{
 			std::cout << _T("Ошибка получения адреса функции") << std::endl;
