4471d3d68141cb9020c97f3b305f749e0a2b8e15
vitiaaap29 <viktor.kyrilovich@gmaol.com>
2013-12-23 05:05:42 +0300

    homework; create threads in pool; manager thread

--- OSiSP/OSiSP/Pool.cpp
+++ OSiSP/OSiSP/Pool.cpp
@@ -5,18 +5,141 @@ using namespace std;
 
 Pool::Pool(int minCountThreads, int maxCounThreads, int maxTimeLife):
 	minCountThreads(minCountThreads), 
-	maxCounThreads(maxCounThreads),
+	maxCountThreads(maxCounThreads),
 	maxTimeLife(maxTimeLife)
 {
-	handlers = new vector<HANDLE>(maxCounThreads);
-}
+	handlers = new map<HANDLE, DWORD>();
+	killThreads = new map<HANDLE, bool>();
+	queueWorks = new vector<WorkItem>();
+	semaphoreFreeThread = CreateSemaphore(NULL, 0, MAX_COUNT_WORKS, NULL);
+	InitializeCriticalSectionAndSpinCount(&criticalSectionForWorks, COUNT_SPIN_CRITICAL_SECTION);
+	InitializeCriticalSectionAndSpinCount(&criticalSectionForTimeLife, COUNT_SPIN_CRITICAL_SECTION);
+	InitializeCriticalSectionAndSpinCount(&criticalSectionForKill, COUNT_SPIN_CRITICAL_SECTION);
 
+	PTHREAD_START_ROUTINE threadWrappedFunction = this->threadFunction;
+	for (int i = 0; i < minCountThreads; i++)
+	{
+		HANDLE temp = CreateThread(NULL, 0, threadWrappedFunction, NULL, 0, NULL);
+		if (temp != NULL)
+		{
+			handlers->at(temp) = 0;
+			killThreads->at(temp) = false;
+		}
+		else
+		{
+			//ярая ошибка
+		}
+	}
+
+	//запускаем поток убийцу
+	PTHREAD_START_ROUTINE threadKiller = this->killer;
+	CreateThread(NULL, 0, threadKiller, NULL, 0, NULL); 
+
+	/*map<HANDLE, bool>::iterator it = killThreads->begin();
+	for (; it != killThreads->end(); it++)
+	{
+		ResumeThread((*it).first);
+	}*/
+}
 
 Pool::~Pool(void)
 {
-	for (int i = 0; i < handlers->size(); i++)
+	map<HANDLE, DWORD>::iterator it = handlers->begin();
+	for (; it != handlers->end(); it++)
 	{
-		CloseHandle(handlers->at(i));
+		CloseHandle((*it).first);
 	}
 	delete handlers;
+	delete queueWorks;
+	CloseHandle(semaphoreFreeThread);
+	DeleteCriticalSection(&criticalSectionForWorks);
+	DeleteCriticalSection(&criticalSectionForTimeLife);
+	DeleteCriticalSection(&criticalSectionForKill);
+}
+
+void Pool::addWorkToQueue(WorkItem work)
+{
+	queueWorks->push_back(work);
+	LONG previosValue;
+	ReleaseSemaphore(semaphoreFreeThread, 1, &previosValue); 
+}
+
+
+
+DWORD WINAPI Pool::threadFunction(void* context)
+{
+	HANDLE currentThreadHandle = getCurrentThreadHandle();
+	//работаем пока хотим, что поток жил или не убиваем его
+	while(true)
+	{
+		//выходим из потока, если ему повелел killer
+		EnterCriticalSection(&criticalSectionForKill);
+		if (killThreads->at(currentThreadHandle) == true)
+		{
+			LeaveCriticalSection(&criticalSectionForKill);
+			break;
+		}
+		LeaveCriticalSection(&criticalSectionForKill);
+
+		//ждём пока нет новой задачи
+		WaitForSingleObject(semaphoreFreeThread, INFINITE);
+
+		//отмечаем, что поток начнёт работать
+		EnterCriticalSection(&criticalSectionForTimeLife);
+		handlers->at(currentThreadHandle) = 0;
+		LeaveCriticalSection(&criticalSectionForTimeLife);
+
+		//атомарно получаем работу и удаляем её из очереди
+		//чтобы несколько потоко не выполняли одно и тоже
+		EnterCriticalSection(&criticalSectionForWorks);
+		WorkItem work = queueWorks->back();
+		queueWorks->pop_back();
+		LeaveCriticalSection(&criticalSectionForWorks);
+
+		//работаем
+		work.function(work.context);
+
+		//время когда поток перестал работать
+		EnterCriticalSection(&criticalSectionForTimeLife);
+		DWORD currentTime = GetTickCount();
+		handlers->at(currentThreadHandle) = currentTime;
+		LeaveCriticalSection(&criticalSectionForTimeLife);
+	}
+
+	return 0;
+}
+
+DWORD WINAPI Pool::killer(PVOID context)
+{
+	int timeWithoutWork = 0;
+	map<HANDLE, DWORD>::iterator it = handlers->begin();
+	map<HANDLE, bool>::iterator itKill = killThreads->begin();
+	for (;it != handlers->end(); it++, itKill++)
+	{
+		if( (*it).second != 0)
+		{
+			timeWithoutWork = GetTickCount() - (*it).second;
+			if (timeWithoutWork >= maxTimeLife)
+			{
+				EnterCriticalSection(&criticalSectionForKill);
+				(*itKill).second = true;
+				LeaveCriticalSection(&criticalSectionForKill);
+			}
+		}
+	}
+	return 0;
+}
+
+HANDLE Pool::getCurrentThreadHandle()
+{
+	HANDLE result;
+	DuplicateHandle(GetCurrentProcess(), GetCurrentThread(),
+		GetCurrentProcess(), &result, 0,
+		FALSE, DUPLICATE_SAME_ACCESS);
+	return result;
+}
+
+void Pool::killAllThread()
+{
+
 }
--- OSiSP/OSiSP/Pool.h
+++ OSiSP/OSiSP/Pool.h
@@ -1,13 +1,43 @@
 #pragma once
 class Pool
 {
-private:
-	int minCountThreads;
-	int maxCounThreads;
-	int maxTimeLife;
-	std::vector<HANDLE> *handlers;
 public:
+	typedef void (*WorkFunction)(void*);
+
+	struct WorkItem
+	{
+		void* context;
+		WorkFunction function;
+		WorkItem(WorkFunction  executableTask, void* context):
+			function(executableTask), context(context){};
+	};
+
 	Pool(int minCountThreads, int maxCounThreads, int maxTimeLife);
 	~Pool(void);
+
+	void addWorkToQueue(WorkItem work);
+	void killAllThread();
+private:
+	const int MAX_COUNT_WORKS = 0xFFFFFFF;
+	const int COUNT_SPIN_CRITICAL_SECTION = 4000;
+	int minCountThreads;
+	int maxCountThreads;
+	int maxTimeLife;
+
+	CRITICAL_SECTION criticalSectionForKill;
+	std::map<HANDLE, bool>* killThreads;
+
+	CRITICAL_SECTION criticalSectionForTimeLife;
+	std::map<HANDLE, DWORD> *handlers;
+
+	std::vector<WorkItem> *queueWorks;
+	CRITICAL_SECTION criticalSectionForWorks;
+	HANDLE semaphoreFreeThread;
+
+	DWORD WINAPI threadFunction(void* context);
+
+	DWORD WINAPI killer(PVOID context);
+
+	HANDLE getCurrentThreadHandle();
 };
 
